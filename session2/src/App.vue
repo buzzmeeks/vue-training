a7687e55519acc209e7af9d2d1fcbf049cba90c4
Aurélie VIOLETTE <orwel.violette@gmail.com>
2019-04-21 16:19:41 +0200

    Add session folder start

---
+++ session2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Un titre</h1>
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from './views/Home'
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
