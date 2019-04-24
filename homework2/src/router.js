7cf4fcae7ae39a6aa4724c0f8e56b268e737c1ed
Aurélie VIOLETTE <orwel.violette@gmail.com>
2019-04-24 17:05:39 +0200

    Solution homework session 2

--- homework2/src/router.js
+++ homework2/src/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Home from '@/views/Home';
 import About from '@/views/About';
 import TypePage from '@/views/TypePage';
+import DetailPage from '@/views/DetailPage';
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,7 @@ const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/type/:name', component: TypePage },
+  { path: '/detail/:pokemonId', component: DetailPage },
 ];
 
 const router = new VueRouter({
